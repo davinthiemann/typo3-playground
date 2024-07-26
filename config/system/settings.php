7d3d3c3a9565b5555882e175a23a405e07a211ef
davin.thiemann <davin.thiemann@hdnet.de>
2024-07-26 11:22:29 +0200

    Add typo3 setup

--- config/system/settings.php
+++ config/system/settings.php
@@ -1,14 +1,39 @@
 <?php
 return [
     'BE' => [
-        'debug' => false,
+        'debug' => true,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$TGdJQlY2bERzbTZOdzdHMg$bSOQH7/fkAtjf8GbSVL7ps8OE/NJJlx/dPfI1BeLbko',
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
             'options' => [],
         ],
     ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'pdo_sqlite',
+                'path' => '/Users/davin.thiemann/Desktop/private-repos/typo3-playground/var/sqlite/cms-3ebeed0f.sqlite',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
     'FE' => [
-        'debug' => false,
+        'debug' => true,
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
             'options' => [],
@@ -21,7 +46,7 @@ return [
                     'writerConfiguration' => [
                         'notice' => [
                             'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                                'disabled' => true,
+                                'disabled' => false,
                             ],
                         ],
                     ],
@@ -57,10 +82,14 @@ return [
                 ],
             ],
         ],
-        'devIPmask' => '',
-        'displayErrors' => 0,
+        'devIPmask' => '*',
+        'displayErrors' => 1,
         'encryptionKey' => '1b5d85d3689f1088bdde7a1c9ed6198d387bdf4a6ebbabb78c0b982ee6f62cd07b13ac19d8e8a589b4f2f4cca1aea918',
-        'exceptionalErrors' => 4096,
+        'exceptionalErrors' => 12290,
+        'sitename' => 'TYPO3',
+        'systemMaintainers' => [
+            1,
+        ],
         'trustedHostsPattern' => '.*.*',
     ],
 ];
